463366364418c000b87a7d12cf11dd8707a57e35
“puttanikhileshreddy” <puttanikhileshreddy@gmail.com>
2018-09-22 14:44:00 +0530

    view find component and routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+//imported components
+ 
 import { ViewComponent } from './view/view.component';
 import { ViewCaseComponent } from './view-case/view-case.component';
 
+// component url paths
 const routes: Routes = [
 {path: 'view-find',component: ViewComponent },
 {path: 'view-case',component: ViewCaseComponent },
@@ -13,4 +17,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+// 'routingComponents'  has declared in app.module.ts so that all components are imported
+
 export const routingComponents = [ViewComponent,ViewCaseComponent]
